ccc3b05cfba766315bec41608ab1516755802684
Michelangelo Mori <328978+blkt@users.noreply.github.com>
2025-02-05 12:54:59 +0100

    Added methods to ChatCompletionRequest.
    These are necessary to fully decouple pipeline steps from message
    representation.

--- src/codegate/providers/fim_analyzer.py
+++ src/codegate/providers/fim_analyzer.py
@@ -24,36 +24,24 @@ class FIMAnalyzer:
         return False
 
     @classmethod
-    def _is_fim_request_body(cls, data: Dict) -> bool:
+    def _is_fim_request_body(cls, data) -> bool:
         """
         Determine from the raw incoming data if it's a FIM request.
         Used by: OpenAI and Anthropic
         """
-        messages = data.get("messages", [])
-        if not messages:
-            return False
-
-        first_message_content = messages[0].get("content")
-        if first_message_content is None:
-            return False
-
-        fim_stop_sequences = ["</COMPLETION>", "<COMPLETION>", "</QUERY>", "<QUERY>"]
-        if isinstance(first_message_content, str):
-            msg_prompt = first_message_content
-        elif isinstance(first_message_content, list):
-            msg_prompt = first_message_content[0].get("text", "")
-        else:
-            logger.warning(f"Could not determine if message was FIM from data: {data}")
-            return False
-        return all([stop_sequence in msg_prompt for stop_sequence in fim_stop_sequences])
+        for txt in data.first_message().text():
+            for stop_sequence in fim_stop_sequences:
+                if stop_sequence not in txt:
+                    return False
+        return True
 
     @classmethod
-    def is_fim_request(cls, request_url_path: str, data: Dict) -> bool:
+    def is_fim_request(cls, request_url_path: str, data) -> bool:
         """
         Determine if the request is FIM by the URL or the data of the request.
         """
         # first check if we are in specific tools to discard FIM
-        prompt = data.get("prompt", "")
+        prompt = data.prompt()
         tools = ["cline", "kodu", "open interpreter"]
         for tool in tools:
             if tool in prompt.lower():
