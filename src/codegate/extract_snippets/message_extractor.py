02a0a2c96c4db85803939125b58be2bae7610fe2
Michelangelo Mori <328978+blkt@users.noreply.github.com>
2025-02-11 16:07:18 +0100

    Fixing base url usage plus other bugs.

--- src/codegate/extract_snippets/message_extractor.py
+++ src/codegate/extract_snippets/message_extractor.py
@@ -288,7 +288,7 @@ class CodeSnippetExtractor(ABC):
         return [
             self._get_snippet_for_match(match)
             for regex in regexes
-            for match in regex.finditer(message)
+            for match in regex.finditer(message.get_text())
         ]
 
     def extract_unique_snippets(self, message: str) -> Dict[str, CodeSnippet]:
