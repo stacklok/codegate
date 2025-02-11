82a5bf5e50d08ece2236d0d58759f2ab0929c3d8
Michelangelo Mori <328978+blkt@users.noreply.github.com>
2025-02-11 13:55:58 +0100

    Added native support for openapi and ollama.
    Also, message/content/text interface to be used throughout the
    pipeline.

---
+++ src/codegate/types/ollama/__init__.py
@@ -0,0 +1,18 @@
+from ._generators import (
+    chat_streaming,
+    generate_streaming,
+)
+
+from ._request_models import (
+    AssistantMessage,
+    ChatRequest,
+    Function,
+    Function,
+    GenerateRequest,
+    Parameters,
+    Property,
+    SystemMessage,
+    ToolCall,
+    ToolDef,
+    UserMessage,
+)
