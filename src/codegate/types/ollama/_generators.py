74b3ac02f90597bf60b35b5a105641f2b914f8c5
Michelangelo Mori <328978+blkt@users.noreply.github.com>
2025-02-11 13:55:58 +0100

    Added native support for openapi and ollama.
    Also, message/content/text interface to be used throughout the
    pipeline.

---
+++ src/codegate/types/ollama/_generators.py
@@ -0,0 +1,73 @@
+import os
+
+import httpx
+import structlog
+
+from ._response_models import (
+    MessageError,
+    StreamingChatCompletion,
+)
+
+
+logger = structlog.get_logger("codegate")
+
+
+# # TODO implement
+# async def generate(request, api_key):
+#     pass
+
+
+async def chat_streaming(request, api_key, base_url="http://localhost:11434"):
+    return streaming(request, api_key, f"{base_url}/api/chat")
+
+
+async def generate_streaming(request, api_key, base_url="http://localhost:11434"):
+    return streaming(request, api_key, f"{base_url}/api/generate")
+
+
+async def streaming(request, api_key, url):
+    payload = request.json(exclude_defaults=True)
+    if os.getenv("CODEGATE_DEBUG_OLLAMA") is not None:
+        print(payload)
+
+    async with httpx.AsyncClient() as client:
+        resp = await client.post(
+            url,
+            content=payload,
+            timeout=300, # TODO this should not be hardcoded
+        )
+
+        # TODO figure out how to best return failures
+        match resp.status_code:
+            case 200:
+                # TODO this loop causes the connection to be kept open
+                # causing read timeouts and blocks (breaks the
+                # typewriter effect), fix this
+                async for message in parser(resp.aiter_lines()):
+                    yield message
+            case 400 | 401 | 403 | 404 | 413 | 429:
+                yield MessageError.model_validate_json(resp.text)
+            # case 500 | 529:
+            #     yield MessageError.model_validate_json(resp.text)
+            case _:
+                logger.error(f"unexpected status code {resp.status_code}", provider="ollama")
+                raise ValueError(f"unexpected status code {resp.status_code}", provider="ollama")
+
+
+async def get_data_lines(lines):
+    count = 0
+    while True:
+        # Every line has a single JSON payload
+        line = await anext(lines)
+        count = count + 1
+        yield line
+    logger.debug(f"Consumed {count} messages", provider="anthropic", count=count)
+
+
+async def parser(lines):
+    messages = get_data_lines(lines)
+    async for payload in messages:
+        item = StreamingChatCompletion.model_validate_json(payload)
+        yield item
+        if item.done:
+            break
