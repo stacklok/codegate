17eae23002a0b419a69cec082a4ca4811bf669b6
Michelangelo Mori <328978+blkt@users.noreply.github.com>
2025-02-14 17:24:45 +0100

    Ported OpenAI and OpenRouter providers.
    Also, tests and fixes.

--- src/codegate/providers/openrouter/provider.py
+++ src/codegate/providers/openrouter/provider.py
@@ -2,66 +2,24 @@ import json
 from typing import Dict
 
 from fastapi import Header, HTTPException, Request
-from litellm import atext_completion
-from litellm.types.llms.openai import ChatCompletionRequest
 
 from codegate.clients.clients import ClientType
 from codegate.clients.detector import DetectClient
 from codegate.pipeline.factory import PipelineFactory
 from codegate.providers.fim_analyzer import FIMAnalyzer
-from codegate.providers.litellmshim import LiteLLmShim, sse_stream_generator
-from codegate.providers.normalizer.completion import CompletionNormalizer
 from codegate.providers.openai import OpenAIProvider
-
-
-class OpenRouterNormalizer(CompletionNormalizer):
-    def __init__(self):
-        super().__init__()
-
-    def normalize(self, data: Dict) -> ChatCompletionRequest:
-        return super().normalize(data)
-
-    def denormalize(self, data: ChatCompletionRequest) -> Dict:
-        """
-        Denormalize a FIM OpenRouter request. Force it to be an accepted atext_completion format.
-        """
-        denormalized_data = super().denormalize(data)
-        # We are forcing atext_completion which expects to have a "prompt" key in the data
-        # Forcing it in case is not present
-        if "prompt" in data:
-            return denormalized_data
-        custom_prompt = ""
-        for msg_dict in denormalized_data.get("messages", []):
-            content_obj = msg_dict.get("content")
-            if not content_obj:
-                continue
-            if isinstance(content_obj, list):
-                for content_dict in content_obj:
-                    custom_prompt += (
-                        content_dict.get("text", "") if isinstance(content_dict, dict) else ""
-                    )
-            elif isinstance(content_obj, str):
-                custom_prompt += content_obj
-
-        # Erase the original "messages" key. Replace it by "prompt"
-        del denormalized_data["messages"]
-        denormalized_data["prompt"] = custom_prompt
-
-        return denormalized_data
+from codegate.types.openai import (
+        ChatCompletionRequest,
+)
 
 
 class OpenRouterProvider(OpenAIProvider):
     def __init__(self, pipeline_factory: PipelineFactory):
-        super().__init__(
-            pipeline_factory,
-            # We get FIM requests in /completions. LiteLLM is forcing /chat/completions
-            # which returns "choices":[{"delta":{"content":"some text"}}]
-            # instead of "choices":[{"text":"some text"}] expected by the client (Continue)
-            completion_handler=LiteLLmShim(
-                stream_generator=sse_stream_generator, fim_completion_func=atext_completion
-            ),
-        )
-        self._fim_normalizer = OpenRouterNormalizer()
+        super().__init__(pipeline_factory)
+        if self._get_base_url() != "":
+            self.base_url = self._get_base_url()
+        else:
+            self.base_url = "https://openrouter.ai/api/v1"
 
     @property
     def provider_route_name(self) -> str:
@@ -74,12 +32,6 @@ class OpenRouterProvider(OpenAIProvider):
         is_fim_request: bool,
         client_type: ClientType,
     ):
-        # litellm workaround - add openrouter/ prefix to model name to make it openai-compatible
-        # once we get rid of litellm, this can simply be removed
-        original_model = data.get("model", "")
-        if not original_model.startswith("openrouter/"):
-            data["model"] = f"openrouter/{original_model}"
-
         return await super().process_request(data, api_key, is_fim_request, client_type)
 
     def _setup_routes(self):
@@ -96,14 +48,12 @@ class OpenRouterProvider(OpenAIProvider):
 
             api_key = authorization.split(" ")[1]
             body = await request.body()
-            data = json.loads(body)
 
-            base_url = self._get_base_url()
-            data["base_url"] = base_url
-            is_fim_request = FIMAnalyzer.is_fim_request(request.url.path, data)
+            req = ChatCompletionRequest.model_validate_json(body)
+            is_fim_request = FIMAnalyzer.is_fim_request(request.url.path, req)
 
             return await self.process_request(
-                data,
+                req,
                 api_key,
                 is_fim_request,
                 request.state.detected_client,
