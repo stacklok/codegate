6989262e55a6092d73246f3976c1f14eaa8d4254
Luke Hinds <luke@stacklok.com>
2025-05-01 20:34:03 +0100

    Lint check

--- migrations/versions/2025_05_01_1917-736fb0c10480_update_empty_provider_endpoint_names_.py
+++ migrations/versions/2025_05_01_1917-736fb0c10480_update_empty_provider_endpoint_names_.py
@@ -5,6 +5,7 @@ Revises: e4c05d7591a8
 Create Date: 2025-05-01 19:17:41.766575
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,8 +13,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '736fb0c10480'
-down_revision: Union[str, None] = 'e4c05d7591a8'
+revision: str = "736fb0c10480"
+down_revision: Union[str, None] = "e4c05d7591a8"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
